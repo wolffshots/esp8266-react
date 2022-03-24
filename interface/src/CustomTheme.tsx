3cea32791d57afc3bdc4839317afb1b8b7a64bf8
jadon <16850875+wolffshots@users.noreply.github.com>
2022-03-24 22:08:24 +0200

    feat: added automatic matching for themes, switched to yarn and added prettier

--- interface/src/CustomTheme.tsx
+++ interface/src/CustomTheme.tsx
@@ -1,38 +1,89 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from "react";
 
-import { CssBaseline } from '@mui/material';
-import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material/styles';
-import { indigo, blueGrey, orange, red, green } from '@mui/material/colors';
+import { CssBaseline } from "@mui/material";
+import {
+	createTheme,
+	responsiveFontSizes,
+	ThemeProvider,
+} from "@mui/material/styles";
+import {
+	indigo,
+	blueGrey,
+	orange,
+	red,
+	green,
+	grey,
+} from "@mui/material/colors";
 
-const theme = responsiveFontSizes(
-  createTheme({
-    palette: {
-      background: {
-        default: "#fafafa"
-      },
-      primary: indigo,
-      secondary: blueGrey,
-      info: {
-        main: indigo[500]
-      },
-      warning: {
-        main: orange[500]
-      },
-      error: {
-        main: red[500]
-      },
-      success: {
-        main: green[500]
-      }
-    }
-  })
+const lightTheme = responsiveFontSizes(
+	createTheme({
+		palette: {
+			background: {
+				default: "#fafafa",
+			},
+			primary: indigo,
+			secondary: blueGrey,
+			info: {
+				main: indigo[500],
+			},
+			warning: {
+				main: orange[500],
+			},
+			error: {
+				main: red[500],
+			},
+			success: {
+				main: green[500],
+			},
+		},
+	})
 );
 
-const CustomTheme: FC = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    {children}
-  </ThemeProvider>
+const darkTheme = responsiveFontSizes(
+	createTheme({
+		palette: {
+			mode: "dark",
+			text: {
+				primary: "#fff",
+				secondary: grey[500],
+			},
+			primary: {
+				main: indigo[500],
+			},
+			secondary: {
+				main: blueGrey[800],
+			},
+			info: {
+				main: indigo[800],
+			},
+			warning: {
+				main: orange[800],
+			},
+			error: {
+				main: red[800],
+			},
+			success: {
+				main: green[800],
+			},
+		},
+	})
 );
+
+const CustomTheme: FC = ({ children }) => {
+	const userDarkColorScheme = window.matchMedia(
+		"(prefers-color-scheme: dark)"
+	).matches;
+	const [darkMode, setDarkMode] = useState(userDarkColorScheme);
+	useEffect(() => {
+		setDarkMode(userDarkColorScheme);
+	}, [userDarkColorScheme]);
+
+	return (
+		<ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+			<CssBaseline />
+			{children}
+		</ThemeProvider>
+	);
+};
 
 export default CustomTheme;
